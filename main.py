2161c5018d2045db2eea093f7f0301ae4afca23d
이재현 <corncornwater@mju.ac.kr>
2026-04-06 14:09:12 +0000

    미션3-이재현-완료

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-# 파일이름 :
-# 작 성 자 :
+
+age = input("나이를 입력하세요 :");
+exist_coupon = input("할인 쿠폰이 있나요? (Y/N) :");
+print("-----------------------------------");
+print("결과 :");
+if ( exist_coupon == 'Y' and int(age) < 13 ) :
+    print("꼬마 VIP 고객님! 팝콘 무료 증정!");
+if ( exist_coupon == 'Y' or int(age) >= 65 ) :
+    print("티켓 요금 : 무료입니다.");
+else :
+    print("티켓 요금 : 15,000원입니다.");
+
